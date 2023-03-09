2ea1750363f9232e5c7e5bdb9ae7d502fa182cb7
yosikari <yosikari@gmail.com>
2023-03-09 16:04:02 +0200

    Finish Portfolio cmp

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,82 @@
 import React from 'react'
 import './portfolio.css'
 
+// import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  // {
+  //   id: 1,
+  //   image: IMG1,
+  //   title: 'Project1',
+  //   github: 'https://github.com',
+  //   demo: 'https://github.com'
+  // },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Project2',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Project3',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project4',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Project5',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Project6',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+]
+
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
-    )
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (<article className='portfolio__item' key={id}>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h2>{title}</h2>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+              </div>
+            </article>)
+          })
+        }
+      </div>
+    </section>
+  )
 }
 
 export default Portfolio
