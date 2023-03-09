510ef6f07b21101239ef9ccdd82e9ac97731393c
yosikari <yosikari@gmail.com>
2023-03-09 18:04:26 +0200

    Finish Contact cmp (using 'EmailJS' lib)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { useRef } from 'react';
 import './contact.css'
 
+import { MdOutlineMailOutline } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+
+import emailjs from 'emailjs-com'
+
 function Contact() {
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_880gtco', 'template_58dzkuj', form.current, 'TeNTAWimHfKAAwV_B')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
-<section id='contact'>Contact</section>  )
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineMailOutline className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>yosikari@gmail.com</h5>
+            <a href="mailto:yosikari@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>yossi.karasik</h5>
+            <a href="https://m.me/yossi.karasik" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+972-528-666064</h5>
+            {/* <a href="https://api.whatsapp.com/send?phone=+972528666064">Send a message</a> */}
+            <a href="https://chatwith.io/s/yossikarasik" target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>)
 }
 
 export default Contact
